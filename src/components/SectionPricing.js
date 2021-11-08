3161f8f2ba9704d0936a391bf9f520552edefeb6
UmerMiB <mohammadumer704@gmail.com>
2021-11-09 00:07:43 +0500

    fixed icons

--- src/components/SectionPricing.js
+++ src/components/SectionPricing.js
@@ -1,33 +1,33 @@
-import React from "react";
-import _ from "lodash";
+import React from 'react'
+import _ from 'lodash'
 
-import { htmlToReact, classNames, markdownify } from "../utils";
-import CtaButtons from "./CtaButtons";
+import { htmlToReact, classNames, markdownify } from '../utils'
+import CtaButtons from './CtaButtons'
 
 export default class SectionPricing extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { isYearly: false };
+    super(props)
+    this.state = { isYearly: false }
   }
 
-  toggleYearlyPackage = () => this.setState({ isYearly: !this.state.isYearly });
+  toggleYearlyPackage = () => this.setState({ isYearly: !this.state.isYearly })
 
   render() {
-    const { isYearly } = this.state;
-    let section = _.get(this.props, "section", null);
+    const { isYearly } = this.state
+    let section = _.get(this.props, 'section', null)
     return (
       <section
-        id={_.get(section, "section_id", null)}
-        className={"pricing-section block pricing-block  " + " outer"}
+        id={_.get(section, 'section_id', null)}
+        className={'pricing-section block pricing-block  ' + ' outer'}
       >
         <div className="block-header pricing-header">
           <div className="background-style"></div>
-          {_.get(section, "title", null) && (
-            <h2 className="block-title">{_.get(section, "title", null)}</h2>
+          {_.get(section, 'title', null) && (
+            <h2 className="block-title">{_.get(section, 'title', null)}</h2>
           )}
-          {_.get(section, "subtitle", null) && (
+          {_.get(section, 'subtitle', null) && (
             <p className="block-subtitle">
-              {htmlToReact(_.get(section, "subtitle", null))}
+              {htmlToReact(_.get(section, 'subtitle', null))}
             </p>
           )}
           {/* <div> */}
@@ -37,15 +37,33 @@ export default class SectionPricing extends React.Component {
                 <input type="checkbox" checked={isYearly} />
                 <span onClick={this.toggleYearlyPackage} class="slider round">
                   <div className="saving-style">
-                  {_.get(section, "yearly_saving", null) && (
-                    _.get(_.get(section, "yearly_saving", null),"image",null))&&(
-                       <img src={_.get(_.get(section, "yearly_saving", null),"image",null)}/>
-                       )
-                    }
-                  {_.get(section, "yearly_saving", null) && (
-                    _.get(_.get(section, "yearly_saving", null),"title",null))&&(
-                      markdownify( _.get(_.get(section, "yearly_saving", null),"title",null)))
-                    }
+                    {_.get(section, 'yearly_saving', null) &&
+                      _.get(
+                        _.get(section, 'yearly_saving', null),
+                        'image',
+                        null,
+                      ) && (
+                        <img
+                          src={_.get(
+                            _.get(section, 'yearly_saving', null),
+                            'image',
+                            null,
+                          )}
+                        />
+                      )}
+                    {_.get(section, 'yearly_saving', null) &&
+                      _.get(
+                        _.get(section, 'yearly_saving', null),
+                        'title',
+                        null,
+                      ) &&
+                      markdownify(
+                        _.get(
+                          _.get(section, 'yearly_saving', null),
+                          'title',
+                          null,
+                        ),
+                      )}
                   </div>
                 </span>
               </span>
@@ -53,133 +71,139 @@ export default class SectionPricing extends React.Component {
           </div>
 
           {/* </div> */}
-          {_.get(section, "pricing_plans", null) && (
+          {_.get(section, 'pricing_plans', null) && (
             <div className="inner">
               <div className="grid">
                 {_.map(
-                  _.get(section, "pricing_plans", null),
+                  _.get(section, 'pricing_plans', null),
                   (plan, plan_idx) => (
                     <div key={plan_idx} className="cell plan">
                       <div
-                        className={classNames("card", {
-                          highlight: _.get(plan, "highlight", null),
+                        className={classNames('card', {
+                          highlight: _.get(plan, 'highlight', null),
                         })}
                       >
                         <div className="plan-header">
-                          {_.get(plan, "title", null) && (
+                          {_.get(plan, 'title', null) && (
                             <h3 className="plan-title">
-                              {_.get(plan, "title", null)}
+                              {_.get(plan, 'title', null)}
                             </h3>
                           )}
-                          {_.get(plan, "subtitle", null) && (
+                          {_.get(plan, 'subtitle', null) && (
                             <div className="plan-subtitle">
-                              {_.get(plan, "subtitle", null)}
+                              {_.get(plan, 'subtitle', null)}
                             </div>
                           )}
-                          {_.get(plan,`price_${isYearly?"yearly": "monthly"}`, null) && (
-                            <div className="plan" style={{paddingTop:!isYearly?"33px":""}}>
-                              {_.get(plan,`price_${isYearly?"yearly": "monthly"}`, null)}
+                          {_.get(
+                            plan,
+                            `price_${isYearly ? 'yearly' : 'monthly'}`,
+                            null,
+                          ) && (
+                            <div
+                              className="plan"
+                              style={{ paddingTop: !isYearly ? '33px' : '' }}
+                            >
+                              {_.get(
+                                plan,
+                                `price_${isYearly ? 'yearly' : 'monthly'}`,
+                                null,
+                              )}
                             </div>
                           )}
-                          {
-                          isYearly&&_.map(
-                            _.get(plan, "saving", null),
-                            (saving) =>
-                              saving.show && (
-                                <div className="plan-price">
-                                  {_.get(saving, "label", null)}
-                                </div>
-                              )
-                          )}
+                          {isYearly &&
+                            _.map(
+                              _.get(plan, 'saving', null),
+                              (saving) =>
+                                saving.show && (
+                                  <div className="plan-price">
+                                    {_.get(saving, 'label', null)}
+                                  </div>
+                                ),
+                            )}
                         </div>
-                        {_.get(plan, "actions", null) &&
-                          !_.get(plan, "institution", null) && (
+                        {_.get(plan, 'actions', null) &&
+                          !_.get(plan, 'institution', null) && (
                             <div className="plan-footer block-buttons">
                               <CtaButtons
                                 {...this.props}
-                                actions={_.get(plan, "actions", null)}
+                                actions={_.get(plan, 'actions', null)}
                                 customStyle={{
-                                  width: "70%",
-                                  height: "56px",
-                                  borderRadius: "8px",
-                                  background: "#006366",
-                                  border: "none",
-                                  marginBottom: "28px",
+                                  width: '70%',
+                                  height: '56px',
+                                  borderRadius: '8px',
+                                  background: '#006366',
+                                  border: 'none',
+                                  marginBottom: '28px',
                                 }}
                                 iconStyle={{
-                                  marginLeft: "10px",
+                                  marginLeft: '10px',
                                 }}
                               />
                             </div>
                           )}
                         <div className="plan-content">
-                          {markdownify(_.get(plan, "details", null))}
+                          {markdownify(_.get(plan, 'details', null))}
                         </div>
-                        {_.get(plan, "actions", null) &&
-                          _.get(plan, "institution", null) && (
+                        {_.get(plan, 'actions', null) &&
+                          _.get(plan, 'institution', null) && (
                             <div className="plan-footer block-buttons">
                               <CtaButtons
                                 {...this.props}
-                                actions={_.get(plan, "actions", null)}
+                                actions={_.get(plan, 'actions', null)}
                                 customStyle={{
-                                  background: "#DEF4EA",
-                                  border: "none",
-                                  color: "#006266",
-                                  borderRadius: "8px",
-                                  width: "241.11px",
-                                  height: "56.13px",
+                                  background: '#DEF4EA',
+                                  border: 'none',
+                                  color: '#006266',
+                                  borderRadius: '8px',
+                                  width: '241.11px',
+                                  height: '56.13px',
                                 }}
                               />
                             </div>
                           )}
                       </div>
                     </div>
-                  )
+                  ),
                 )}
               </div>
             </div>
           )}
         </div>
-        {_.get(section, "remark_feature", null) && (
+        {_.get(section, 'remark_feature', null) && (
           <div className="remark-feature-div">
-            {_.map(_.get(section, "remark_feature", null), (item, rem_id) => (
+            {_.map(_.get(section, 'remark_feature', null), (item, rem_id) => (
               <div>
                 <img src={item.image} />
-                {_.get(item, "title", null) && (
-                  <span>{markdownify(_.get(item, "title", null))}</span>
+                {_.get(item, 'title', null) && (
+                  <span>{markdownify(_.get(item, 'title', null))}</span>
                 )}
               </div>
             ))}
           </div>
         )}
         <div className="contact-us">
-          {_.get(section, "contact", null) && (
+          {_.get(section, 'contact', null) && (
             <div>
-              {_.get(_.get(section, "contact", null), "information", null) &&
+              {_.get(_.get(section, 'contact', null), 'information', null) &&
                 markdownify(
-                  _.get(_.get(section, "contact", null), "information", null)
+                  _.get(_.get(section, 'contact', null), 'information', null),
                 )}
             </div>
           )}
-          {_.get(_.get(section, "contact", null), "actions", null) && (
+          {_.get(_.get(section, 'contact', null), 'actions', null) && (
             <CtaButtons
               {...this.props}
-              actions={_.get(_.get(section, "contact", null), "actions", null)}
+              actions={_.get(_.get(section, 'contact', null), 'actions', null)}
               customStyle={{
-                background: "#FFF",
-                border: "none",
-                borderRadius: "8px",
-                color: "#006266",
+                background: '#FFF',
+                border: 'none',
+                borderRadius: '8px',
+                color: '#006266',
               }}
             />
-            // <button>
-            //   {" "}
-            //   {_.get(section, "contact_email", null)}{" "}
-            //   <img src={require("./../images/arrow.png")} />{" "}
-            // </button>
           )}
         </div>
       </section>
-    );
+    )
   }
 }
