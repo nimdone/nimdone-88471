6ffa1b73b60fc9b48156632e71e2e4a30c6bdb00
Karan Krishnani <krkrishn@thoughtworks.com>
2022-12-12 07:34:25 -0500

    Revert "Adds Facebook Pixel for Ad campaigns"
    This reverts commit 38ac2946b208cbb3ce3743a93a4a228679bf4bd5.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,26 +7,19 @@
 const React = require("react");
 const withPrefix = require("./src/utils/withPrefix").default;
 
-exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
-  setHeadComponents([
-    <React.Fragment>
-      <script src={withPrefix("js/fb-pixel.js")} />
-      <noscript>
-        <img
-          height="1"
-          width="1"
-          style="display:none"
-          src="https://www.facebook.com/tr?id=521088480063794&ev=PageView&noscript=1"
-        />
-      </noscript>
-    </React.Fragment>,
-  ]);
+exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
 
-  setPostBodyComponents([
-    <React.Fragment>
-      <script src={withPrefix("js/plugins.js")} />
-      <script src={withPrefix("js/init.js")} />
-      <script src={withPrefix("js/main.js")} />
-    </React.Fragment>,
-  ]);
+    setHeadComponents([
+        
+    ]);
+
+    setPostBodyComponents([
+        <React.Fragment>
+            <script src={withPrefix('js/plugins.js')}/>
+            <script src={withPrefix('js/init.js')}/>
+            <script src={withPrefix('js/main.js')}/>
+            
+        </React.Fragment>
+    ]);
+
 };
